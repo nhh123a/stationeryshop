0c7890cf76b17b494a1059f34203277bc5ed2966
nhh123a <nhh123a@gmail.com>
2022-06-07 22:23:21 +0700

    first commit

--- app/Http/Controllers/Web/Menu/MenuWebController.php
+++ app/Http/Controllers/Web/Menu/MenuWebController.php
@@ -36,6 +36,17 @@ class MenuWebController extends Controller
     function brand($id){
         $title = Brand::select('brand_title')->find($id)->brand_title;
         $data = Product::select('product_id','product_title','product_image','product_price')->where('product_brand',$id)->paginate(9);
+        if(isset($_GET['sort_by'])){
+            if($_GET['sort_by'] == "price"){
+                $data = $data->sortBy('product_price');
+            }
+            if($_GET['sort_by'] == "title"){
+                $data = $data->sortBy('product_title');
+            }
+            if($_GET['sort_by'] == "new"){
+                $data = $data->sortByDesc('product_id');
+            }
+        }
         return view('web.category.home',[
             'title' => $title,
             'titlew31' => $title,
